9f7780a741e0a5126134f2cc5e66574626dee7fe
Jalon-Tech <71904112+Jalon-Tech@users.noreply.github.com>
2025-06-26 13:44:58 -0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA66sbVnP3CNVpdTUz13MuLwemiObpR-QM",
+  authDomain: "techforge-academy.firebaseapp.com",
+  projectId: "techforge-academy",
+  storageBucket: "techforge-academy.firebasestorage.app",
+  messagingSenderId: "724281790220",
+  appId: "1:724281790220:web:1698209a0840d83e5ac329",
+  measurementId: "G-TCQY5VY8M8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
